b36099917257220709f68bb994d326322156af29
Aimini05 <aimi.nakatani05@gmail.com>
2026-04-13 13:02:59 +0200

    Editing the the SPM_grupp3.build.cs file so it can use Enhancedinput

--- Source/SPM_grupp3/SPM_grupp3.Build.cs
+++ Source/SPM_grupp3/SPM_grupp3.Build.cs
@@ -8,7 +8,7 @@ public class SPM_grupp3 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
