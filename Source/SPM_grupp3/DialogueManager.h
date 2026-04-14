3b63349a1116e291d459bf0c91d0a8476e0b083a
Aimini05 <aimi.nakatani05@gmail.com>
2026-04-14 11:32:21 +0200

    Adding DialogueManager files

---
+++ Source/SPM_grupp3/DialogueManager.h
@@ -0,0 +1,35 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DialogueManager.generated.h"
+
+class UUserWidget;
+
+UCLASS()
+class SPM_GRUPP3_API ADialogueManager : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ADialogueManager();
+
+	UFUNCTION(BlueprintCallable)
+	void ShowMessage(const FText& Message);
+
+	UFUNCTION(BlueprintCallable)
+	void HideMessage();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Dialogue")
+	TSubclassOf<UUserWidget> DialogueWidgetClass;
+
+	UPROPERTY()
+	TObjectPtr<UUserWidget> DialogueWidgetInstance;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Dialogue")
+	FText CurrentMessage;
+};
