95d101ce7ac90f9f5ddd3a8efae8de5fec5e8e38
Aimini05 <aimi.nakatani05@gmail.com>
2026-04-14 13:23:53 +0200

    Updating DialogueManager
    Need to add movement lock to the player, button to advance through dialogs

--- Source/SPM_grupp3/DialogueManager.cpp
+++ Source/SPM_grupp3/DialogueManager.cpp
@@ -1,8 +1,11 @@
-
 #include "DialogueManager.h"
-#include "Blueprint/UserWidget.h"
 #include "DialogueWidgetBase.h"
-#include "Engine/Engine.h"
+#include "CharacterAimi.h"
+
+#include "Blueprint/UserWidget.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+
 ADialogueManager::ADialogueManager()
 {
 	PrimaryActorTick.bCanEverTick = false;
@@ -28,8 +31,6 @@ void ADialogueManager::ShowMessage(const FText& Message)
 {
 	CurrentMessage = Message;
 
-	UE_LOG(LogTemp, Warning, TEXT("Dialogue Message: %s"), *Message.ToString());
-
 	if (DialogueWidgetInstance)
 	{
 		DialogueWidgetInstance->SetVisibility(ESlateVisibility::Visible);
@@ -42,5 +43,87 @@ void ADialogueManager::HideMessage()
 	if (DialogueWidgetInstance)
 	{
 		DialogueWidgetInstance->SetVisibility(ESlateVisibility::Hidden);
+	}
+}
+
+void ADialogueManager::StartDialogue(const TArray<FText>& InLines)
+{
+	if (InLines.IsEmpty())
+	{
+		return;
+	}
+
+	ActiveDialogueLines = InLines;
+	CurrentDialogueIndex = 0;
+	bDialogueActive = true;
+
+	SetPlayerMovementEnabled(false);
+	ShowCurrentDialogueLine();
+}
+
+void ADialogueManager::AdvanceDialogue()
+{
+	if (!bDialogueActive)
+	{
+		return;
+	}
+
+	CurrentDialogueIndex++;
+
+	if (!ActiveDialogueLines.IsValidIndex(CurrentDialogueIndex))
+	{
+		EndDialogue();
+		return;
+	}
+
+	ShowCurrentDialogueLine();
+}
+
+void ADialogueManager::EndDialogue()
+{
+	bDialogueActive = false;
+	CurrentDialogueIndex = 0;
+	ActiveDialogueLines.Empty();
+
+	HideMessage();
+	SetPlayerMovementEnabled(true);
+}
+
+bool ADialogueManager::IsDialogueActive() const
+{
+	return bDialogueActive;
+}
+
+void ADialogueManager::ShowCurrentDialogueLine()
+{
+	if (!ActiveDialogueLines.IsValidIndex(CurrentDialogueIndex))
+	{
+		return;
+	}
+
+	ShowMessage(ActiveDialogueLines[CurrentDialogueIndex]);
+}
+
+void ADialogueManager::SetPlayerMovementEnabled(bool bEnabled)
+{
+	ACharacterAimi* PlayerCharacter = Cast<ACharacterAimi>(
+		UGameplayStatics::GetPlayerCharacter(GetWorld(), 0)
+	);
+
+	if (!PlayerCharacter)
+	{
+		return;
+	}
+
+	if (UCharacterMovementComponent* MoveComp = PlayerCharacter->GetCharacterMovement())
+	{
+		if (bEnabled)
+		{
+			MoveComp->SetMovementMode(MOVE_Walking);
+		}
+		else
+		{
+			MoveComp->DisableMovement();
+		}
 	}
 }
--- Source/SPM_grupp3/DialogueManager.h
+++ Source/SPM_grupp3/DialogueManager.h
@@ -21,8 +21,23 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void HideMessage();
 
+	UFUNCTION(BlueprintCallable)
+	void StartDialogue(const TArray<FText>& InLines);
+
+	UFUNCTION(BlueprintCallable)
+	void AdvanceDialogue();
+
+	UFUNCTION(BlueprintCallable)
+	void EndDialogue();
+
+	UFUNCTION(BlueprintCallable)
+	bool IsDialogueActive() const;
+
 protected:
 	virtual void BeginPlay() override;
+
+	void ShowCurrentDialogueLine();
+	void SetPlayerMovementEnabled(bool bEnabled);
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Dialogue")
 	TSubclassOf<UDialogueWidgetBase> DialogueWidgetClass;
@@ -32,4 +47,13 @@ protected:
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Dialogue")
 	FText CurrentMessage;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Dialogue")
+	bool bDialogueActive = false;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Dialogue")
+	TArray<FText> ActiveDialogueLines;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Dialogue")
+	int32 CurrentDialogueIndex = 0;
 };
