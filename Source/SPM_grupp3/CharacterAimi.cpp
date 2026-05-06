755fac2811a969cb490c491f028a1ef85edbd615
Maddeximous <andersson.madeleine00@gmail.com>
2026-05-06 14:33:48 +0200

    CamerFixedPrayers

--- Source/SPM_grupp3/CharacterAimi.cpp
+++ Source/SPM_grupp3/CharacterAimi.cpp
@@ -147,6 +147,8 @@ void ACharacterAimi::Move(const FInputActionValue& Value)
 	{
 		return;
 	}
+	
+	ACameraDirector* Direcotr = Cast<ACameraDirector>(UGameplayStatics::GetActorOfClass(GetWorld(), ACameraDirector::StaticClass()));
 
 	// Use camera/controller yaw to make movement relative to the current view.
 	// This makes W feel like "forward on screen" in a normal third-person test camera.
