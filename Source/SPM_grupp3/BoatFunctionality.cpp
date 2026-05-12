3098760c173ec37a425d6fae3c08178b3452920d
PaulaPiana <p.gebath@gmx.de>
2026-05-12 13:32:43 +0200

    Added my boat collision idea as comment to BoatFunctionality.cpp

--- Source/SPM_grupp3/BoatFunctionality.cpp
+++ Source/SPM_grupp3/BoatFunctionality.cpp
@@ -22,10 +22,17 @@ ABoatFunctionality::ABoatFunctionality()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	
+	// ---
 	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
     
 	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisualMesh"));
 	MeshComponent->SetupAttachment(RootComponent);
+	// ---
+	
+	/*
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisualMesh"));
+	RootComponent = MeshComponent;
+	 */
 	
 	// ----------------------------------- CAMERA -----------------------------------
 
