c8c189aabb4033dda0c2b75da92cb3f54a2472b7
Maddeximous <andersson.madeleine00@gmail.com>
2026-05-18 14:02:12 +0200

    Fix c++

--- Source/SPM_grupp3/BTService_CheckForPlayer.cpp
+++ Source/SPM_grupp3/BTService_CheckForPlayer.cpp
@@ -3,20 +3,3 @@
 
 #include "BTService_CheckForPlayer.h"
 
-UBTService_CheckForPlayer::UBTService_CheckForPlayer()
-{
-	NodeName = "CheckForPlayer";
-}
-
-void UBTService_CheckForPlayer::SetIsPlayerOnBoat(bool InIsPlayerOnBoat)
-{
-	IsPlayer
-}
-
-UBTService_CheckForPlayer::IsPlayerOnBoat(UBehaviorTreeComponent& OwnerComp, bool InWater)
-{
-	Blackboard = OwnerComp.GetBlackboardComponent();
-	if (Blackboard) return;
-	
-	Blackboard->SetValueAsBool(IsPlayerInWater.SelectedKeyName, InWater);
-}
